7cf6200f7b4b0d3998d48ea325443f146b8249c0
andreygal <andreygalper@gmail.com>
2019-03-31 14:17:59 -0400

    next

--- cc2.js
+++ cc2.js
@@ -24,24 +24,22 @@ if (johnAvg === mikeAvg || maryAvg === johnAvg || mikeAvg === maryAvg) {
             console.log('John wins 1st place, Mike and Mary are 2nd');
         }
     }
-}
-
-} else {
+}  else {
      if (johnAvg > mikeAvg) {
        if (mikeAvg > maryAvg) {
            console.log('1st place John; 2nd place Mike, 3rd place Mary'); 
-       } else if (maryAvg < johnAvg) {
+       } else if (maryAvg > mikeAvg) {
            console.log('1st place John; 2nd place Mary, 3rd place Mike'); 
        } else {
-           console.log('1st place Mary; 2nd place John; 3rd place Mike')
+           console.log('1st place Mary; 2nd place John; 3rd place Mike');
        }
     } else {
-       if (mikeAvg > maryAvg) {
+       if (maryAvg > johnAvg) {
+           console.log('1st place Mike; 2nd place Mary, 3rd place John'); 
+       } else if (maryAvg < mikeAvg) {
            console.log('1st place Mike; 2nd place John, 3rd place Mary'); 
-       } else if (maryAvg < johnAvg) {
-           console.log('1st place John; 2nd place Mary, 3rd place Mike'); 
        } else {
-           console.log('1st place Mary; 2nd place John; 3rd place Mike')
+           console.log('1st place Mary; 2nd place Mike; 3rd place John');
        }
     }
 }
