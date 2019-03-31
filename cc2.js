e9228652f00615239b62ddc834cd4cecc0ca78c4
andreygal <andreygalper@gmail.com>
2019-03-31 14:30:10 -0400

    edited hmtl

--- cc2.js
+++ cc2.js
@@ -1,6 +1,6 @@
-var johnAvg = (89 + 120 + 103) / 3;
-var mikeAvg = (116 + 94 + 123) / 3;
-var maryAvg = (97 + 134 + 105) / 3;
+var johnAvg = (153 + 95 + 29) / 3;
+var mikeAvg = (11 + 112 + 48) / 3;
+var maryAvg = (161 + 15 + 38) / 3;
 
 if (johnAvg === mikeAvg || maryAvg === johnAvg || mikeAvg === maryAvg) {
     if (johnAvg === mikeAvg && mikeAvg === maryAvg) {
@@ -28,10 +28,10 @@ if (johnAvg === mikeAvg || maryAvg === johnAvg || mikeAvg === maryAvg) {
      if (johnAvg > mikeAvg) {
        if (mikeAvg > maryAvg) {
            console.log('1st place John; 2nd place Mike, 3rd place Mary'); 
-       } else if (maryAvg > mikeAvg) {
-           console.log('1st place John; 2nd place Mary, 3rd place Mike'); 
+       } else if (maryAvg > johnAvg) {
+           console.log('1st place Mary; 2nd place John, 3rd place Mike'); 
        } else {
-           console.log('1st place Mary; 2nd place John; 3rd place Mike');
+           console.log('1st place John; 2nd place Mary; 3rd place Mike');
        }
     } else {
        if (maryAvg > johnAvg) {
@@ -43,3 +43,5 @@ if (johnAvg === mikeAvg || maryAvg === johnAvg || mikeAvg === maryAvg) {
        }
     }
 }
+
+console.log("John: " + johnAvg + " Mike: " + mikeAvg + " Mary: " + maryAvg); 
